8e6fbec0bfb50bcaf2a108832226efc72c62f292
nandaprasetya <nandaprasetya712@gmail.com>
2025-04-26 23:54:06 +0800

    feat: add watchlist variabel and getter

--- src/account/Users.java
+++ src/account/Users.java
@@ -1,7 +1,11 @@
 package account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Users extends Accounts{
     private double balance;
+    private static List<String> watchlist = new ArrayList<>();
 
     public Users(String id, String username, String password, String email, String role, double balance) {
         super(id, username, password, email, "USER");
@@ -10,5 +14,9 @@ public class Users extends Accounts{
 
     public double getBalance() {
         return balance;
+    }
+
+    public static List<String> getWatchlist() {
+        return watchlist;
     }
 }
