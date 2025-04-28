7d81e0027d5e500df354b8277cbb66da14aee6c1
depmkeu <candradevi2016@gmail.com>
2025-04-28 11:12:00 +0700

    Fix: Update main flow in Main.java

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import Routes.Routes;
+import Account.Login;
+import Account.Accounts;
+import Account.Session;
+import Account.Users;
+import Account.Admin;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Login login = new Login();
+
+        System.out.println("||==================================================================||");
+        System.out.println("||                      WELCOME INVESTIA                            ||");
+        System.out.println("||                   AYO INVESTASI SEKARANG!!                       ||");
+        System.out.println("||==================================================================||");
+        System.out.println();
+        System.out.println("||      Mohon masukkan Username dan Password untuk melanjutkan      ||");
+        System.out.println("||------------------------------------------------------------------||");
+        System.out.print("|| USERNAME : ");
+        String username = scanner.nextLine();
+        System.out.print("|| PASSWORD : ");
+        String password = scanner.nextLine();
+        System.out.println();
+
+        Accounts user = login.login(username, password);
+        if (user == null) {
+            System.out.println("||==================================================================||");
+            System.out.println("||                          LOGIN GAGAL                             ||");
+            System.out.println("||      Username atau password Anda salah. Silakan coba lagi!       ||");
+            System.out.println("||==================================================================||");
+        } else {
+            if (user instanceof Users) {
+                Routes.userRoutes();
+            } else if (user instanceof Admin) {
+                Routes.adminRoutes();
+            }
+        }
+        scanner.close();
+    }
+}
