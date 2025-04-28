78f694f858a078cb892499ecd324d4b41cf97d6e
depmkeu <candradevi2016@gmail.com>
2025-04-28 11:44:29 +0700

    Add: Accounts class under Account Package

---
+++ src/Account/Accounts.java
@@ -0,0 +1,49 @@
+package Account;
+
+public class Accounts {
+    protected String id;
+    protected String username;
+    protected String password;
+    protected String email;
+    protected String role;
+
+    public Accounts(String id, String username, String password, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean checkPassword(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
