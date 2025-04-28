acf6ab7aebc831a7393fbeaf8415b67307e2e804
depmkeu <candradevi2016@gmail.com>
2025-04-28 12:29:14 +0700

    feat: add Stocks entity class with attributes for bonds data

---
+++ src/Securities/Stocks.java
@@ -0,0 +1,45 @@
+package Securities;
+
+import java.util.ArrayList;
+
+public class Stocks extends Securities{
+    private ArrayList<Double> priceHistory;
+    private String sector;
+
+    public Stocks(String code, String name, double firstPrice, String sector) {
+        super(code, name, firstPrice);
+        this.priceHistory = new ArrayList<>();
+        this.priceHistory.add(firstPrice);
+        this.sector = sector;
+    }
+
+    public ArrayList<Double> getPriceHistory() {
+        return priceHistory;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public double getPreviousPrice() {
+        if (priceHistory.size() < 2) return price;
+        return priceHistory.get(priceHistory.size() - 2);
+    }
+
+    public double getCurrentPrice() {
+        if (!priceHistory.isEmpty()) {
+            return priceHistory.get(priceHistory.size() - 1);
+        }
+        return price;
+    }
+
+    public String getPriceChangePercentage() {
+        if (getPreviousPrice() == 0) return "0";
+        double change = (getCurrentPrice() - getPreviousPrice()) / getPreviousPrice() * 100;
+        return String.format("%.2f%%", change);
+    }
+
+    public double getPriceChange() {
+        return getCurrentPrice() - getPreviousPrice();
+    }
+}
