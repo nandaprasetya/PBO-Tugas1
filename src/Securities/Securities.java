d1e02a17ce2d50c5ecb85e636c93c833ddcc94dd
depmkeu <candradevi2016@gmail.com>
2025-04-28 12:10:37 +0700

    feat: add base Securities class for stocks and SBNs

---
+++ src/Securities/Securities.java
@@ -0,0 +1,25 @@
+package Securities;
+
+public class Securities {
+    protected String code;
+    protected String name;
+    protected double price;
+
+    public Securities(String code, String name, double price) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
